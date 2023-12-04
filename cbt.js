a56021b562f967f4a9a586b1bb6ef4191c44c0ab
iwasbornofthis <iwasbornofthis@gmail.com>
2023-12-04 13:04:13 +0900

    글꼴

--- cbt.js
+++ cbt.js
@@ -1,7 +1,30 @@
 window.onload = function () {
 
+    let startTime = Date.now();
+    let timerId;
+    let elapsedTime;
+    let minutes;
+    let seconds;
 
-    
+    // 시간 업데이트 함수
+    function updateTime() {
+        const currentTime = Date.now();
+        elapsedTime = currentTime - startTime; // 밀리초 단위 시간 차이
+
+        // 분과 초로 변환
+        minutes = Math.floor(elapsedTime / 60000);
+        seconds = Math.floor((elapsedTime % 60000) / 1000);
+
+        // 시간 표시
+        document.getElementById("timer").innerText = `시간: ${minutes} 분 ${seconds} 초`;
+    }
+
+    // 1초마다 시간 업데이트
+    timerId = setInterval(updateTime, 1000);
+
+    // 시간 표시
+   
+
 
     const cbtForm = document.getElementById('cbtForm');
     const questionContainer = document.getElementById('question-container');
@@ -21,7 +44,7 @@ window.onload = function () {
         const currentQuestion = quiz[questionIndex];
         if (currentQuestion) {
             const shuffledChoices = shuffleArray(Object.entries(currentQuestion.choices));
-    
+
 
             questionContainer.innerHTML = `<div class="question-header">${currentQuestion.question}</div>`;
 
@@ -43,6 +66,8 @@ window.onload = function () {
     }
 
     function displayRandomQuestion() {
+        const minutes = Math.floor(elapsedTime / 60000);
+        const seconds = ((elapsedTime % 60000) / 1000).toFixed(0);
         const questionIndexes = Object.keys(quiz);
         const unansweredQuestions = questionIndexes.filter(q => !answeredQuestions[q]);
 
@@ -50,8 +75,10 @@ window.onload = function () {
             currentQuestionIndex = unansweredQuestions[Math.floor(Math.random() * unansweredQuestions.length)];
             displayQuestion(currentQuestionIndex);
         } else {
+            clearInterval(timerId);
             const answeredCount = Object.keys(answeredQuestions).length;
             questionContainer.innerHTML = `
+            <p>시간: ${minutes} 분 ${seconds} 초</p>초
           <p>모든 문제를 완료하였습니다. 감사합니다!</p>
           <p>총 ${answeredCount}개의 문제를 풀었습니다.</p>
           <button id="restart-button" onclick="restartQuiz()">다시 시작</button>
@@ -92,15 +119,20 @@ window.onload = function () {
         const resultsContainer = document.getElementById('results-container');
         const totalQuestions = Object.keys(quiz).length;
         const wrongCount = Object.values(answeredQuestions).filter(val => val === 'wrong').length;
-    
+
         if (wrongCount === 0) {
             alert('축하합니다! 모든 문제를 맞췄습니다.');
+            clearInterval(timerId);
+
         } else {
             alert(`총 ${totalQuestions}문제 중 ${wrongCount}문제를 틀렸습니다.`);
+            clearInterval(timerId);
+
         }
-    
+
         // 결과 컨테이너 내용 설정
         resultsContainer.innerHTML = `
+        문제를 푸는데 걸린 시간: ${minutes} 분 ${seconds} 초
           ${wrongCount > 0 ? '<button id="review-wrong-answers">오답노트 보기</button>' : ''}
           <button id="restart-button">다시 시작</button>
         `;
@@ -111,21 +143,21 @@ window.onload = function () {
         // 이벤트 리스너 설정
         const restartButton = document.getElementById('restart-button');
         restartButton.addEventListener('click', restartQuiz);
-    
+
         // wrongNoteContainer를 다시 추가하기 전에 기존 부모 노드에서 제거
         if (wrongNoteContainer && wrongNoteContainer.parentNode) {
             wrongNoteContainer.parentNode.removeChild(wrongNoteContainer);
         }
-    
+
         // resultsContainer에 wrongNoteContainer를 추가
         if (wrongNoteContainer) {
             resultsContainer.appendChild(wrongNoteContainer);
         }
-    
+
         resultsContainer.classList.remove('hidden');
         resultsContainer.classList.add('slide-in-text');
     }
-    
+
 
 
 
@@ -154,7 +186,7 @@ window.onload = function () {
         let wrongNoteContainer = document.createElement('div');
         wrongNoteContainer.id = 'wrong-note-container';
         wrongNoteContainer.innerHTML = '<h2>오답노트</h2>';
-    
+
         Object.keys(wrongAnswers).forEach(questionIndex => {
             const question = quiz[questionIndex];
             wrongNoteContainer.innerHTML += `<p><strong>문제: </strong>${question.question}</p>`;
@@ -166,7 +198,7 @@ window.onload = function () {
                 wrongNoteContainer.innerHTML += `<div class="${divClass}">${choiceValue}</div><br>`;
             });
         });
-    
+
         const resultsContainer = document.getElementById('results-container');
         // 기존에 wrongNoteContainer가 존재한다면 제거합니다.
         const existingContainer = document.getElementById('wrong-note-container');
@@ -176,8 +208,7 @@ window.onload = function () {
         // 새로운 wrongNoteContainer를 추가합니다.
         resultsContainer.appendChild(wrongNoteContainer);
     }
-    
+
     displayRandomQuestion();
 
 }
-
